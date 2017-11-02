e81ddbfa1535e0932be533f17fe58409d39c402f
misha gupta <guptamisha88@gmail.com>
2017-11-02 14:51:43 +0530

    Ledger report export updated data

--- ShineYatraAdmin/Script/Report.js
+++ ShineYatraAdmin/Script/Report.js
@@ -15,17 +15,24 @@
                 buttons: [
                     {                       
                         extend: 'excelHtml5',
+                        footer: true,
                         text : 'Export to Excel',
                         filename: function(){
                             var d = new Date();
                             var n = d.getTime();
                             return 'LedgerReport_' + n;
+                        },
+                        exportOptions: {
+                            columns: [1,4,5,6,7,8,9,10,11]
                         }
-                    }
+                   }
                 ],
                 "aaData": jsonobject,
-                "aoColumns": [
-                    {"mData": "wallet_txn_date"},
+                "aoColumns": [                   
+                    { "mData": "wallet_txn_date"},                    		
+                    { "mData": "statement_date"},
+                    { "mData": "service_id" },
+                    { "mData": "ledger_name" },
                     { "mData": "op_ledger_name" },
                     { "mData": "service_name" },
                     { "mData": "wallet_txn_id" },
@@ -39,29 +46,19 @@
                     },
                     { "mData": "remarks" },
                     { "mData": "ref_txn_id" }
-                ],
-                //initComplete: function() {
-                //    this.api().columns().every(function() {
-                //        var column = this;
-                //        column
-                //            .search('', true, false)
-                //            .draw();
-                //    });
-                //},
-                "columnDefs": [
-                    { className: "text-right", "targets": [3,4,5,6,8] },      
-                ],
+                ],                                
                 "footerCallback": function () {
-                    var api = this.api(),
-                     columns = [4, 5];
-                    for (var i = 0; i < columns.length; i++) {
-                        $('tfoot th').eq(columns[i]).html(api.column(columns[i], { filter: 'applied' }).data().sum());                       
-                    }
-                    $('tfoot th').eq(3).html('Total:');
-                    var last_row = api.row(':last').data();
-                    $('tfoot th').eq(6).html(last_row.balance + " " + last_row.drcr);
+                    var api = this.api();
+                     $(api.column(6).footer()).html('Total:');
+                     $(api.column(7).footer()).html(api.column(7, { filter: 'applied' }).data().sum());
+                     $(api.column(8).footer()).html(api.column(8, { filter: 'applied' }).data().sum());                     
+                     var last_row = api.row(':last').data();
+                     $(api.column(9).footer()).html(last_row.balance + " " + last_row.drcr);
                 },
- 
+                "columnDefs": [
+                                   { className: "text-right", "targets": [6,7, 8, 9,11] },
+                                   { "targets": [0, 2, 3], "visible": false }
+                ],
                 "destroy": true,
                 "paging": false,
                 "ordering": false,
