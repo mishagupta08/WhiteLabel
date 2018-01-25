601ae4584c673e662beccd7ed1043ded39b68cc1
misha gupta <guptamisha88@gmail.com>
2018-01-25 17:46:16 +0530

    flight list filter

--- ShineYatraAdmin/Script/flight.js
+++ ShineYatraAdmin/Script/flight.js
@@ -15,13 +15,14 @@ $(document).ready(function () {
         "bPaginate": false,
         "order":[],
         "aoColumnDefs": [      
-             {'bSortable': false, 'aTargets': [0,1,2,4,5,7]},             
+             { 'bSortable': false, 'aTargets': [0, 1, 2, 3, 4] },
+             { "bVisible": false, "aTargets": [0] },
              {
                  "aTargets": [0],
                  "mData": function (source, type, val) {
                      if (type === 'set') {
-                         var airlinename = val.split("<breakit>");
-                         source.airlinedisplay = val,
+                         var airlinename = val.split("breakit");                         
+                         source.airlinedisplay = airlinename[1];
                          source.airlinefilter = airlinename[0];
                          return;
                      }
@@ -34,72 +35,73 @@ $(document).ready(function () {
                      // 'sort', 'type' and undefined all just use the integer
                      return source.airlinefilter;
                  }
-             },
+             
+           },
          {
-             "aTargets": [3],
+             "aTargets": [5],
              "mData": function (source, type, val) {
                  if (type === 'set') {                     
-                     source.durationdisplay = val;
-                     source.duration = parseFloat(val);
+                     source.stopdisplay = val;
+                     source.stop = parseFloat(val);
                      return;
                  }
                  else if (type === 'display') {
-                     return source.durationdisplay;
+                     return source.stopdisplay;
                  }
                  else if (type === 'filter') {
-                     return source.duration;
+                     return source.stop;
                  }
                  // 'sort', 'type' and undefined all just use the integer
-                 return source.duration;
-             }
-         },
-         {
-             "aTargets": [6],
-             "mData": function (source, type, val) {
-                 if (type === 'set') {
-                     var arr = val.split("<breakit>");
-                     source.pricedisplay = val;
-                     source.price = parseFloat(arr[0]);
-                     return;
-                 }
-                 else if (type === 'display') {
-                     return source.pricedisplay;
-                 }
-                 else if (type === 'filter') {
-                     return source.price;
-                 }
-                 // 'sort', 'type' and undefined all just use the integer
-                 return source.price;
+                 return source.stop;
              }
          }
+         //{
+         //    "aTargets": [6],
+         //    "mData": function (source, type, val) {
+         //        if (type === 'set') {
+         //            var arr = val.split("<breakit>");
+         //            source.pricedisplay = val;
+         //            source.price = parseFloat(arr[0]);
+         //            return;
+         //        }
+         //        else if (type === 'display') {
+         //            return source.pricedisplay;
+         //        }
+         //        else if (type === 'filter') {
+         //            return source.price;
+         //        }
+         //        // 'sort', 'type' and undefined all just use the integer
+         //        return source.price;
+         //    }
+         //}
         ],
-
         stateSave: true
     });     
 
+    oTable.column(5).visible(false);
     var airlineNames = oTable.column(0).data().unique().sort();
     var airlinechkbox = "";
     for (var i = 0 ; i < airlineNames.length; i++)
     {
-        airlinechkbox += "<label><input type=\"checkbox\" checked name=\"type\" class=\"airlinetype\" value=\"" + airlineNames[i] + "\">&nbsp;" + airlineNames[i] +"</label><br />";
+        airlinechkbox += "<div class=\"checkbox\"><label><input type=\"checkbox\" checked name=\"type\" class=\"airlinetype i-check\" value=\"" + airlineNames[i] + "\">&nbsp;" + airlineNames[i] + "</label></div><br />";
     }
 
     $("#AirlineNameCheckbox").html(airlinechkbox);
 
-    var stops = oTable.column(4).data().unique().sort();
+    var stops = oTable.column(5).data().unique().sort();
     var stopschkbox = "";
     for (var i = 0 ; i < stops.length; i++) {
-        stopschkbox += "<label><input type=\"checkbox\" checked name=\"Stops\" class=\"Stops\" value=\"" + stops[i] + "\">&nbsp;" + stops[i] + "</label><br />";
+        stopschkbox += "<div class=\"checkbox\"><label><input type=\"checkbox\" checked name=\"Stops\" class=\"Stops i-check\" value=\"" + stops[i] + "\">&nbsp;" + stops[i] + "</label></div><br />";
     }
 
     $("#stopsCheckbox").html(stopschkbox);
        
-    var price = oTable.column(6).data().unique().join(',');  
+    //var price = oTable.column(6).data().unique().join(',');  
 
-    var ids = price.split(',').map(parseFloat);
+    //var ids = price.split(',').map(parseFloat);
     
-    maxvalue = Math.max.apply(Math, ids);
-    minvalue = Math.min.apply(Math, ids);
+    //maxvalue = Math.max.apply(Math, ids);
+    //minvalue = Math.min.apply(Math, ids);
 
    
     $(".airlinetype").change(function () {
@@ -107,6 +109,7 @@ $(document).ready(function () {
         var types = $('input:checkbox[name="type"]:checked').map(function () {
             return this.value;
         }).get().join('|');
+        
         //filter in column 0, with an regex, no smart filtering, no inputbox,not case sensitive
         oTable
         .columns(0)
@@ -121,7 +124,7 @@ $(document).ready(function () {
         }).get().join('|');
         //filter in column 0, with an regex, no smart filtering, no inputbox,not case sensitive
         oTable
-        .columns(4)
+        .columns(5)
         .search('^' + types + '$', true, false)
         .draw();
     });
